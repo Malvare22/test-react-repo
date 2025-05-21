47d315f50e1ec9253c45e12ed7e33e51f1ca2ab8
JulianQuirozG <75459275+JulianQuirozG@users.noreply.github.com>
2025-05-21 12:40:55 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
-import { ConfigEnv, defineConfig, loadEnv, UserConfigExport } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from "@vitejs/plugin-react";
 
 /**
@@ -11,7 +11,7 @@ import react from "@vitejs/plugin-react";
  * The `base` option ensures that all static assets and routes are correctly resolved.
  * It must match the subdirectory you're hosting the app under.
  */
-export default ({ mode }: ConfigEnv): UserConfigExport  => {
+export default ({ mode })  => {
   const env = loadEnv(mode, process.cwd());
   //console.log('VITE_BASE_PATH:', env.VITE_BASE_PATH);  // Verifica la carga
   //console.log('VITE_PORT:', env.VITE_PORT);  // Verifica la carga
